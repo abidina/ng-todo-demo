376a019b33ab7ce571d8add4c82d3e6aab9c245f
Abbie Holder <abigail.c.holder@gmail.com>
2016-05-18 21:35:33 -0500

    cleared staging

--- app/app.js
+++ app/app.js
@@ -1,1 +1,12 @@
-console.log("hello");
+// console.log("hello");
+
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", function($scope) {
+  $scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
+})
+
+app.controller("TodoCtrl", function($scope) {
+  $scope.welcome = "hello";
+
+})
