755b1f1b3e21b45ab0cf8567c18ed94fa5e41e4f
Abbie Holder <abigail.c.holder@gmail.com>
2016-05-21 10:11:50 -0500

    button functionality

--- app/app.js
+++ app/app.js
@@ -9,4 +9,15 @@ app.controller("NavCtrl", function($scope) {
 app.controller("TodoCtrl", function($scope) {
   $scope.welcome = "hello";
 
+  $scope.showListView = true;
+
+  $scope.newItem = function() {
+    console.log("you clicked new item");
+    $scope.showListView = false;
+  };
+
+  $scope.allItem = function() {
+    console.log("you clicked all item");
+    $scope.showListView = true;
+  };
 });
