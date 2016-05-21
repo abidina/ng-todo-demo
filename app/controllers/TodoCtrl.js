2d27e27d522b477b2b4e4cb16ecdd77db65579d7
Abbie Holder <abigail.c.holder@gmail.com>
2016-05-21 12:46:56 -0500

    breaking out angular

---
+++ app/controllers/TodoCtrl.js
@@ -0,0 +1,57 @@
+
+app.controller("TodoCtrl", function($scope) {
+  $scope.welcome = "hello";
+  $scope.showListView = true;
+  $scope.task= "";
+  $scope.newTask = {};
+
+  $scope.items= [
+    {
+      id: 0,
+      task: "mow the lawn",
+      isCompleted: true,
+      dueDate: "12/5/17",
+      assignedTo: "greg",
+      location: "Zoe's house",
+      urgency: "low",
+      dependencies: ["sunshine, clippers, water, hat, headphones"]
+    },
+    {
+      id: 1,
+      task: "grade quizzes",
+      isCompleted: false,
+      dueDate: "12/5/15",
+      assignedTo: "joe",
+      location: "NSS",
+      urgency: "high",
+      dependencies: ["tissues, wifi, vodka"]
+    },  
+    {
+      id: 2,
+      task: "take a nap",
+      isCompleted: false,
+      dueDate: "5/21/16",
+      assignedTo: "zoe",
+      location: "Zoe's house",
+      urgency: "medium",
+      dependencies: ["hammock, cat, pillow, blanket"]
+    }
+  ];
+
+  $scope.newItem = function() {
+    console.log("you clicked new item");
+    $scope.showListView = false;
+  };
+
+  $scope.allItem = function() {
+    console.log("you clicked all item");
+    $scope.showListView = true;
+  };
+
+  $scope.addNewItem = function(){
+    $scope.newTask.isCompleted = false;
+    $scope.newTask.id = $scope.items.length;
+    $scope.items.push($scope.newTask);
+    $scope.newTask = "";
+  };
+});
