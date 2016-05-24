09e2d650348498c89016319d6ea935d0ac1e8d86
Abbie Holder <abigail.c.holder@gmail.com>
2016-05-23 19:36:01 -0500

    working with delete button

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,16 +1,27 @@
 app.controller("ItemListCtrl", function($scope, $http) {
   $scope.items= [];
+  var getItems = function() {
+    $http.get("./data/items.json") // <-- http.get vs. $.ajax(method:GET); works bc this is being called from index.html (so you don't have to move out of any folders)
+      .success(function(itemObject) {
+        var itemCollection = itemObject.items;
 
-  $http.get("./data/items.json") // <-- http.get vs. $.ajax(method:GET); works bc this is being called from index.html (so you don't have to move out of any folders)
-    .success(function(itemObject) {
-      var itemCollection = itemObject.items;
-
-      Object.keys(itemCollection).forEach(function(key) { // <-- returns an array of all keys in the object; by putting (key) in parentheses, you store it
-        itemCollection[key].id=key;
-        $scope.items.push(itemCollection[key]); //<-- this grabs the item you're looping through and gives it an id & pushes it into an array
+        Object.keys(itemCollection).forEach(function(key) { // <-- returns an array of all keys in the object; by putting (key) in parentheses, you store it
+          itemCollection[key].id=key;
+          $scope.items.push(itemCollection[key]); //<-- this grabs the item you're looping through and gives it an id & pushes it into an array
+        });
       });
-      
+  };
+  
+  getItems();
 
-    });
+    $scope.itemDelete= function(itemId) {
+      console.log("itemId", itemId);
+      $http
+        .delete(`firebaseurlhere-${itemId}`)
+        .success(function(response) {
+          $scope.items = [];
+          getItems();
+        });
+    };
 
 });
